7a158fe9e0e5aa539ecb8ca03ec2f4f209d1d25a
san232 <76610724+san232@users.noreply.github.com>
2025-03-10 22:37:52 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+    int main()
+    {
+        int A;
+        int B;
+        
+        cin >> A >> B;
+        int ans1 = A + B;
+        int ans2 = A - B;
+        int ans3 = A * B;
+        int ans4 = A / B;
+        int ans5 = A % B;
+        
+        cout << ans1 << endl;
+        cout << ans2 << endl;
+        cout << ans3 << endl;
+        cout << ans4 << endl;
+        cout << ans5 << endl;
+    }
